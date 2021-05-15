6b677a04f17006ba1351f8c4e934322322576e34
podbal <podbal@gmail.com>
2021-05-15 02:24:01 +0200

    w03 Main.java minor changes

--- w03/src/com/greenfoxacademy/Main.java
+++ w03/src/com/greenfoxacademy/Main.java
@@ -4,5 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("TEST");
+    // HelloWorld string kiírása a konzolra/terminál ablakba
+        System.out.println("Hello, World!");
+    // Egy soros comment
+
+    /*
+    Több soros
+    comment blokk
+    */
+
+    //print out integer
+        System.out.println(42);
+    //print out floating point number
+        System.out.println(3.141592);
+        
+
+
+
+
+
     }
 }
