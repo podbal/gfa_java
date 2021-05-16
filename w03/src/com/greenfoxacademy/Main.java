50cd58828d3a6044361b1992ff5bd1d4fcd32eaf
podbal <podbal@gmail.com>
2021-05-16 21:56:56 +0200

    Ternary, break, continue

--- w03/src/com/greenfoxacademy/Main.java
+++ w03/src/com/greenfoxacademy/Main.java
@@ -32,13 +32,18 @@ public class Main {
             System.out.println("There is no food for you!");
         }
 
+        // Ternary operator
+        int number = 3;
+        String msg = (number == 0) ? "The number is even!" : "The number is odd!";
+        System.out.println(msg);
+
         // Loops / ciklusok
         //while
         System.out.println("While loop");
         int index = 0;
-        while (index <= 100) {
+        while (index <= 10) {
             System.out.print(index + ", "); //Terminal 0 => 100
-            if (index == 100) {
+            if (index == 10) {
                 System.out.println("End of while loop");
             }
             ++index; //or index +=1;
@@ -46,9 +51,9 @@ public class Main {
 
         //for
         System.out.println("For loop");
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.print(i + ", "); //Terminal 0 => 100
-            if (i == 100) {
+            if (i == 10) {
                 System.out.println("End of for loop");
             }
         }
@@ -59,7 +64,29 @@ public class Main {
         do {
             System.out.print(index + ", ");
             index += 1;
-        } while (index <= 100);
+        } while (index <= 10);
+        System.out.println("End of do while loop");
+
+        // break 5 ciklus utan
+        index = 0;
+        do {
+            System.out.print(index + ", ");
+            index += 1;
+            if (index == 5) {
+                System.out.println("Break after 5th loop");
+                break;
+            }
+        } while (index <= 10);
+
+        // continue, csak paros szamok kiiratasa
+        index = 0;
+        do {
+            index += 1;
+            if (index % 2 != 0) {
+                continue;
+            }
+            System.out.print(index + ", ");
+        } while (index < 10);
 
 
     }
