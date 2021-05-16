f3ceeaeea9509b0af06c8d6d997dc23e85162757
podbal <podbal@gmail.com>
2021-05-17 00:23:48 +0200

    1st integer Array example

--- w03/Arrays/src/com/greenfoxacademy/Main.java
+++ w03/Arrays/src/com/greenfoxacademy/Main.java
@@ -3,6 +3,16 @@ package com.greenfoxacademy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //
+        int length = 10;
+        int[] numbers = new int[length];
+        System.out.println(numbers); // [I@2503dbd3
+        System.out.println("Array lenght: " + numbers.length); // Array lenght: 10
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+
+        }
+
     }
 }
