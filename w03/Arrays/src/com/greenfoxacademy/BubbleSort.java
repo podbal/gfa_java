f8a55a537e67ea358ee5e53dbb0b54f6cb5f12ef
podbal@gmail.com <podbal@gmail.com>
2021-05-19 13:32:32 +0200

    Arrays.sort was demonstrated.

--- w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
+++ w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
@@ -1,2 +1,14 @@
-package com.greenfoxacademy;public class BubbleSort {
+package com.greenfoxacademy;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        //System.out.printf("Hello");
+        int[] numbers = {12, 545, 345345, -223412, -2, 2132, 753, 7, 8};
+        System.out.println("Numbers tömb: " + Arrays.toString(numbers));
+        //sorbarendezés beépített paranccsal
+        Arrays.sort(numbers);
+        System.out.println("Tömb sorba rendezve: " + Arrays.toString(numbers));
+    }
 }
