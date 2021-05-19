3b19c8e204b46b860179dbc170ebd842eb9caea9
podbal@gmail.com <podbal@gmail.com>
2021-05-19 14:42:00 +0200

    Basic BubbleSort was implemeneted.

--- w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
+++ w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
@@ -8,7 +8,20 @@ public class BubbleSort {
         int[] numbers = {12, 545, 345345, -223412, -2, 2132, 753, 7, 8};
         System.out.println("Numbers tömb: " + Arrays.toString(numbers));
         //sorbarendezés beépített paranccsal
-        Arrays.sort(numbers);
-        System.out.println("Tömb sorba rendezve: " + Arrays.toString(numbers));
+        //Arrays.sort(numbers); //csak kipróbálásra kellet
+        //System.out.println("Tömb sorba rendezve: " + Arrays.toString(numbers));
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                int first = numbers[j];
+                int second = numbers[j + 1];
+                if (first > second) {
+                    numbers[j] = second;
+                    numbers[j + 1] = first;
+                }
+            }
+        }
+        System.out.println("BubbleSort rendezés: " + Arrays.toString(numbers));
+
     }
 }
