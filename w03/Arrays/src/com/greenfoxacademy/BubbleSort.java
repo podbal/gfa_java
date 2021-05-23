cceb5c9f3d38787fdea40716507d26e469ff9e58
podbal <podbal@gmail.com>
2021-05-23 12:01:08 +0200

    Optimised BubbleSort added by Krisztian.

--- w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
+++ w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         //System.out.printf("Hello");
         int[] numbers = {12, 545, 345345, -223412, -2, 2132, 753, 7, 8};
-        System.out.println("Numbers tömb: " + Arrays.toString(numbers));
+        System.out.println("Numbers tömb       : " + Arrays.toString(numbers));
         //sorbarendezés beépített paranccsal
         //Arrays.sort(numbers); //csak kipróbálásra kellet
         //System.out.println("Tömb sorba rendezve: " + Arrays.toString(numbers));
@@ -40,23 +40,24 @@ public class BubbleSort {
         */
 
         /* optimalizált SWAP, 1 egy változóval */
-        int i, j;
+
         int lastSwap = numbers.length;
         do {
             int newN = 0;
-            for (j = 0; j < lastSwap - 1; j++) {
-                System.out.println("J: " + numbers[j] + " J+1: " + numbers[j + 1]);
-                if (numbers[j] > numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
-                    newN = j;
+            for (int i = 1; i <= lastSwap - 1; i++) {
+                System.out.println("I-1: " + numbers[i - 1] + " I: " + numbers[i]);
+                if (numbers[i - 1] > numbers[i]) {
+                    int tmp = numbers[i - 1];
+                    numbers[i - 1] = numbers[i];
+                    numbers[i] = tmp;
+                    newN = i;
                 }
             }
             lastSwap = newN;
-        } while (lastSwap > 1);
+        } while (lastSwap >= 1);
 
         System.out.println("BubbleSort rendezés: " + Arrays.toString(numbers));
+        System.out.println("Várt eredmény      : [-223412, -2, 7, 8, 12, 545, 753, 2132, 345345]");
     }
 }
 
