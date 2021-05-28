8437462611d6618c1ab284427497c117b12c507f
podbal <podbal@gmail.com>
2021-05-28 12:32:34 +0200

    More comments added.

--- w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
+++ w03/Arrays/src/com/greenfoxacademy/BubbleSort.java
@@ -2,13 +2,10 @@ package com.greenfoxacademy;
 
 import java.util.Arrays;
 
-public class BubbleSort {
-    public static void main(String[] args) {
+public class BubbleSort { //Bubble sort osztály létrehozása
+    public static void main(String[] args) { //main a Java kód belépési pontja
         //System.out.printf("Hello");
-        int[] numbers = {12, 545, 345345, -223412, -2, 2132, 753, 7, 8};
-        System.out.println("Numbers tömb       : " + Arrays.toString(numbers));
         //sorbarendezés beépített paranccsal
-        //Arrays.sort(numbers); //csak kipróbálásra kellet
         //System.out.println("Tömb sorba rendezve: " + Arrays.toString(numbers));
 
         /* Basic BubbleSort
@@ -39,25 +36,34 @@ public class BubbleSort {
         System.out.println("BubbleSort rendezés: " + Arrays.toString(numbers));
         */
 
+        int[] numbers = {12, 545, 345345, -223412, -2, 2132, 753, 7, 8}; //egész számokat tartalmazó tomb inicializálása, literal
+        System.out.println("Numbers tömb       : " + Arrays.toString(numbers)); //tömb kiírása
+        //Arrays.sort(numbers); //csak kipróbálásra kellet, ezzel kiváltható de nem megérthető a Bubble sort
         /* optimalizált SWAP, 1 egy változóval */
 
-        int lastSwap = numbers.length; //ciklus max futása a tömb hosszával egyenlő
-        do {
-            int newLastSwap = 0; //eddig futott a for ciklus, itt volt az utolsó swap, nem kell ennél tovább futtatni a ciklust
-            for (int i = 1; i <= lastSwap - 1; i++) { //a tömb első elemétől a tömb-1 szer fut a ciklus
+        int lastSwap = numbers.length; //egész változó ami ciklus max futása = tömb hosszával = az utolsó csere helye
+        do
+        { //hátultesztelő cilus, elfut 1x majd ellenőrzi a feltételt, táncos példában ez kezdi mindíg előrről az összehasonlítást
+            int newLastSwap = 0; //átmeneti egész típ. változó = itt volt az utolsó swap, max eddig kell futtatni a ciklust,
+            // azért kell, hogy ne írjuk felül a latSwap változót mert annak az értékét használjuk a belső for ciklus futása közben
+            for (int i = 1; i <= lastSwap - 1; i++) { //mindíg csak az utolsó csere helyéig fut a ciklus: lastSwap - 1
+                //az utolsó táncosok akik már rendezettek voltak, megfordultak, rajtuk már nem futott tovább feleslegesen a sort
+                // -1 a 0-tól indexelés miatt kell, i++ felfelé számláló ciklus
+                // a tömb első elemétől i=1 indul, igy megspórolható egy 1 fölösleges futás, ahol a 0. elem = lenne önmagával
                 System.out.println("i " + i + " I-1: " + numbers[i - 1] + " I: "
-                        + numbers[i]); //ciklusszám I-1 és I tömbelemek állapotok
-                if (numbers[i - 1] > numbers[i]) { //ha i-1 nagyobb mint i akkor
-                    int tmp = numbers[i - 1]; // csere/swap
-                    numbers[i - 1] = numbers[i];
-                    numbers[i] = tmp;
+                        + numbers[i]); //ciklusszám + I-1 + I állapotai, csak a futás megértéséhez kell
+                if (numbers[i - 1] > numbers[i]) { //ha (i-1) előbbi elem nagyobb mint (i) utóbbi elem akkor
+                    int tmp = numbers[i - 1]; // tmp átmeneti változó segítségével jön a csere/swap tmp-be << előbbi elemet
+                    numbers[i - 1] = numbers[i]; //mivel előbbi elembe << útóbbi elemet
+                    numbers[i] = tmp; //előbbi elembe << tmp-t
                     newLastSwap = i; //itt volt az utolsó swap, ennél többször már nem kell futnia a következő for cilkusnak
                     System.out.println("newLastSwap:" + newLastSwap); //newLastSwap állapota
                 }
             }
             lastSwap = newLastSwap; //utolsó swap az i-edik futásnál, max ennyiszer kell már csak futnia a for-nak
             System.out.println("lastSwap:" + lastSwap); //lastSwap állapota
-        } while (lastSwap >= 1); //amíg lastswap kisebb vagy egyenlő mint 1, tehát az utolsó swap 0 vagy 1-nél volt
+        } while (lastSwap >= 1); //ellenőrizzük, hogy az utolsó csere helye >=1, ha már nem igaz akkor nem kell többet futnia a ciklusnak
+        //táncosok példáján látható amikor az elején 4-en megfordulnak, ez itt valósul meg a kódban
 
         System.out.println("BubbleSort rendezés: " + Arrays.toString(numbers));
         System.out.println("Várt eredmény      : [-223412, -2, 7, 8, 12, 545, 753, 2132, 345345]");
